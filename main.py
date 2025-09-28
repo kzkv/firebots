5331c14df89dd9867777d952d8b3393dc63c2275
Tom Kazakov <tomkazakov@gmail.com>
2025-09-28 17:58:18 -0400

    cell-based fire line planner

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pygame
 import numpy as np
 from fire_bitmap import load_fire_bitmap
 from obstacles import place_trees
+from planning import fireline_cells
 from render import World
 
 # 6x3' field, 72x36", scale is 1" -> 1'
@@ -13,20 +14,28 @@ COLS = 72
 ROWS = 36
 CELL_SIZE = 30
 hud_height = CELL_SIZE
+FIRELINE_FIRE_GAP = 2  # A marging between the fireline and the fire cells
+FIRELINE_OBSTACLE_GAP = 1  # Hugging the obstacles
 
 TREE_COUNT = 50
 
 rng = np.random.default_rng()
 pygame.init()
+pygame.display.set_caption("Firebots")
 world = World(ROWS, COLS, CELL_SIZE)
 
 running = True
 
 # Ingest fire bitmap
 fire_surface, fire_grid = load_fire_bitmap("fire1.png", world.cols, world.rows)
+# fire_surface, fire_grid = load_fire_bitmap("fire2.png", world.cols, world.rows)
 
 # Generate obstacles
 tree_grid = place_trees(world.cols, world.rows, count=TREE_COUNT, rng=rng)
+
+# Establish fire line
+fireline_grid = fireline_cells(fire_grid, gap=FIRELINE_FIRE_GAP, obstacles=tree_grid)
+
 
 while running:
     for e in pygame.event.get():
@@ -39,6 +48,7 @@ while running:
     world.render_fire_cells(fire_grid)
     world.render_trees(tree_grid)
     world.render_tree_sprites(tree_grid, rng)
+    world.render_fireline_cells(fireline_grid)
 
     pygame.display.flip()
     world.clock.tick(60)
